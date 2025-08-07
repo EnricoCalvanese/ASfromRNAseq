28239b61fd963491ee90b43751936bdd68ee71f0
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2025-08-07 15:24:51 -0700

    Update hisat_index.sh

--- Min/hisat_index.sh
+++ Min/hisat_index.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=hisat2_index
 #SBATCH --account=fc_rnaseq
 #SBATCH --partition=savio2
 #SBATCH --qos=savio_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=01:00:00
+#SBATCH --time=00:45:00
 #SBATCH --mail-user=enrico_calvane@berkeley.edu
 #SBATCH --mail-type=ALL
+#SBATCH --output=build_HISAT_index.log
 
 # Define input files
 FASTA_FILE=/global/scratch/users/enricocalvane/minRNAseq/as/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
